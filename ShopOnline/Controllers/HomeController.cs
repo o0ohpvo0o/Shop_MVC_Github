95c830fcf4ebcb83e8ec6c73811d2fc9da1c9dda
TRAN <p.n.tran999@gmail.com>
2018-10-13 13:38:30 +0100

    Commit Client Site ngay 1

--- ShopOnline/Controllers/HomeController.cs
+++ ShopOnline/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model.Dao;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,32 @@ namespace ShopOnline.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            ViewBag.Slides = new SlideDao().GetAll();
+            var product = new ProductDao();
+            ViewBag.NewProduct = product.GetNewProduct(4);
+            ViewBag.FeatureProduct = product.GetFeatureProduct(4);
             return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult MainMenu()
+        {
+            var model = new MenuDao().ListByGroupId(1);
+            return PartialView(model);
+        }
+
+        [ChildActionOnly]
+        public ActionResult TopMenu()
+        {
+            var model = new MenuDao().ListByGroupId(2);
+            return PartialView(model);
+        }
+
+        [ChildActionOnly]
+        public ActionResult Footer()
+        {
+            var model = new FooterDao().GetFooter();
+            return PartialView(model);
         }
     }
 }
