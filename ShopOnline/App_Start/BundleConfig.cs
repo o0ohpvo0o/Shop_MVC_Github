4ed78a37b69751ec563f9fdbfdffd70cc55cdd3b
TRAN <p.n.tran999@gmail.com>
2018-10-29 15:07:28 +0000

    Archieved JS and CSS

--- ShopOnline/App_Start/BundleConfig.cs
+++ ShopOnline/App_Start/BundleConfig.cs
@@ -8,26 +8,52 @@ namespace ShopOnline
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/morris").Include(
-                        "~/Scripts/morris-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/AdminJsCore").Include(
+                      "~/Assets/admin/vendor/jquery/jquery.js",
+                      "~/Scripts/jquery.unobtrusive-ajax.min.js",
+                      "~/Assets/admin/vendor/bootstrap/js/bootstrap.min.js",
+                      "~/Assets/admin/vendor/metisMenu/metisMenu.min.js",
+                      "~/Assets/admin/js/plugin/ckfinder/ckfinder.js",
+                      "~/Assets/admin/js/plugin/ckeditor/ckeditor.js",
+                      "~/Assets/admin/vendor/raphael/raphael.min.js",
+                      "~/Assets/admin/vendor/morrisjs/morris.min.js",
+                      "~/Assets/admin/dist/js/sb-admin-2.js",
+                      "~/Assets/admin/dist/js/Alert-admin.js"
+                      ));
 
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/ClientJsCore").Include(
+                "~/Assets/client/js/jquery-3.3.1.min.js",
+                "~/Assets/client/js/bootstrap.min.js",
+                "~/Assets/client/js/popper.min.js",
+                "~/Assets/client/js/jquery.auto-complete.min.js",
+                "~/Assets/client/js/move-top.js",
+                "~/Assets/client/js/easing.js",
+                "~/Assets/client/js/startstop-slider.js"
+                ));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/ClientJsController").Include(
+                "~/Assets/client/js/controller/baseController.js"
+                ));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new StyleBundle("~/bundles/AdminCssCore")
+                      .Include("~/Assets/admin/vendor/bootstrap/css/bootstrap.min.css", new CssRewriteUrlTransform())
+                      .Include("~/Assets/admin/vendor/font-awesome/css/font-awesome.min.css", new CssRewriteUrlTransform())
+                      .Include("~/Assets/admin/vendor/metisMenu/metisMenu.min.css", new CssRewriteUrlTransform())
+                      .Include("~/Assets/admin/dist/css/sb-admin-2.css", new CssRewriteUrlTransform())
+                      .Include("~/Assets/admin/vendor/morrisjs/morris.css", new CssRewriteUrlTransform())
+                      );
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            bundles.Add(new StyleBundle("~/bundles/ClientCssCore")
+                .Include("~/Assets/client/css/bootstrap.css", new CssRewriteUrlTransform())
+                .Include("~/Assets/client/css/bootstrap-social.css", new CssRewriteUrlTransform())
+                .Include("~/Assets/client/css/all.css", new CssRewriteUrlTransform())
+                .Include("~/Assets/client/css/font-awesome.min.css", new CssRewriteUrlTransform())
+                .Include("~/Assets/client/css/style.css", new CssRewriteUrlTransform())
+                .Include("~/Assets/client/css/slider.css", new CssRewriteUrlTransform())
+                .Include("~/Assets/client/css/jquery.auto-complete.css", new CssRewriteUrlTransform())
+                );
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
